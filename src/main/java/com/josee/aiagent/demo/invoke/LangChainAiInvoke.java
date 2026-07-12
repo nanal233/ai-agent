94d64e2e1747d33c2411294e1339522fbea8f86a
Josee <jhee0141@student.monash.edu>
2026-07-12 18:59:05 +1000

    更改

---
+++ src/main/java/com/josee/aiagent/demo/invoke/LangChainAiInvoke.java
@@ -0,0 +1,15 @@
+package com.josee.aiagent.demo.invoke;
+
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+
+public class LangChainAiInvoke {
+    public static void main(String[] args) {
+        ChatLanguageModel qwenChatModel = QwenChatModel.builder()
+                .apiKey(testApiKey.API_KEY)
+                .modelName("qwen-plus")
+                .build();
+        String result = qwenChatModel.chat("你好，我是Josee");
+        System.out.println(result);
+    }
+}
