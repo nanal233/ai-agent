4148de317c93631329cf7a1aac058740f149f62a
Josee <jhee0141@student.monash.edu>
2026-07-12 17:37:14 +1000

    更改

--- src/main/java/com/josee/aiagent/demo/invoke/SpringAiAiInvoke.java
+++ src/main/java/com/josee/aiagent/demo/invoke/SpringAiAiInvoke.java
@@ -1,4 +1,12 @@
 package com.josee.aiagent.demo.invoke;
 
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.model.ChatModel;
+
+/**
+ * Spring AI 框架调用 AI 大模型
+ */
 public class SpringAiAiInvoke {
+    @Resource
+    private ChatModel dashscopeChatModel;
 }
